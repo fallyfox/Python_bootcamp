ba7b9d83f341afe41d80ba4dca51a304988f5c60
Joseph Ogbu <57672199+fallyfox@users.noreply.github.com>
2020-02-15 20:17:49 +0100

    Create loops_exercises.py

---
+++ loops_exercises.py
@@ -0,0 +1,11 @@
+n_list = [2.2,5,9,3.8]
+#1: print every member of the n_list in an integer state ****hint: use for loops
+#2: print only members that are greater than 4 ***hint: use for loop
+
+ball = 'bounce'
+times = 5
+#1: keep printing 'bounce' as long as the variable 'times' is not greater than 5
+
+#if someone is 25 years old, calculate how many hours he has lived
+#then, count down hour by hour until the first hour he lived
+
